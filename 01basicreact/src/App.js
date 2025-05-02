8c75560f9ba052992bbe8566ee582164c0221a50
Yash-01907 <yashpandey1907@gmail.com>
2025-05-02 08:57:47 +0530

    Tried to update the basic src and folder and changed what is bbeing returned

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello World | Yash pandey</h1>
+      <p>To aaj 2 may 2025 ko maine react padhna start kiya abhi chai aur react series ka first lecture dekh ke khatam kiya <br></br>
+      Aur kya kya sikha aaj maine
+      </p>
+      <p>
+        Iss lecture mein maine sikha ki kaise react project start karte hai <br></br>
+        <br></br>
+        Ek tarika hota hai npx create-react-app projectName ka use karke. This takes more time isme comparatively jyada dependencies download hoti hai isiliye time jyada lagta hai aur hum projectName ko number se bhi start kar sakte hai. Isse jab project banta hai to usme node modules already installed hota hai
+        <br></br>
+        <br></br>
+        Phir dusra tarika sikha vo tha vite ka use karke. Usme hum likhte hai npm vite create vite@latest ka use karke karte hai. Isme starting mein project name nahi dena hota hai. Isse banne projects mein node module nahi hota hai project banne ke baad hume npm i karke saare dependencies install karna padta hai
+      </p>
+    </div>
+  );
+}
+
+export default App;
