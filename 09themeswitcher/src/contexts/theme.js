02b0d975430586828f9e7f11ba5058fb9b67bcc4
Yash-01907 <yashpandey1907@gmail.com>
2025-05-24 12:37:17 +0530

    Made todo using context api and local storage

--- 09themeswitcher/src/contexts/theme.js
+++ 09themeswitcher/src/contexts/theme.js
@@ -1,9 +1,9 @@
 import { createContext,useContext } from "react";
 
 export const ThemeContext = createContext({
-    themeMode:"light",
-    darkTheme: ()=>{},
-    lightTheme:()=>{}
+    // themeMode:"light",
+    // darkTheme: ()=>{},
+    // lightTheme:()=>{}
 })
 
 
